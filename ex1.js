299655f1bbfa2699518c5b32318cafb87c5bd61a
Giriprakasho <giriprakash.s051105@gmail.com>
2025-06-25 23:39:35 +0530

    dom.html,ex1.js,Printing.html,random.html,sum.js

---
+++ ex1.js
@@ -0,0 +1,5 @@
+var product="Apple"
+var PRICE=10000;
+var tax=270
+console.log(product)
+console.log(PRICE*tax)
---
+++ sum.js
@@ -0,0 +1,7 @@
+let a=10;
+var b=10;
+function sum()
+{
+    console.log(a+b);
+}
+sum()
